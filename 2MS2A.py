f84f8e12fa2f1da54b3b995f9ab40b4c42adda5d
Toaster <38060892+torstenheavner@users.noreply.github.com>
2020-02-24 09:41:01 -0700

    Created main 2MS2A.py

---
+++ 2MS2A.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import json
+
+from discord.ext import commands
+
+
+def clear(): return os.system("cls")
+
+
+sys.path.append("T:/all")
+bot = commands.Bot(command_prefix="2mb.")
+
+with open("data.json", "r") as dataFile:
+    cogs = json.loads(file.read())["cogs"]
+    
+for extension in cogs:
+    bot.load_extension(extension)
+    print("%s LOADED." % extension)
+
+
+@bot.event
+async def on_ready():
+    clear()
+    print("\nTOO MUCH STUFF TO AUTOMATE (BETA)\nONLINE\n\n")
+
+
+@bot.command(brief="Check if the bot is responding.")
+async def ping(ctx):
+    await ctx.send("Pong!")
+    print("%s PINGED THE BOT." % ctx.author.name.upper())
+
+
+@bot.command(name="reload", brief="Reload all or one of the bots cogs.")
+async def _reload(ctx, cog="all"):
+    log = []
+    with open("data.json", "r") as dataFile:
+        cogs = json.loads(file.read())["cogs"]
+    if cog == "all":
+        for extension in cogs:
+            try:
+                bot.unload_extension(extension)
+                bot.load_extension(extension)
+                log.append("**%s** reloaded successfully." % extension)
+            except:
+                bot.load_extension(extension)
+                log.append("**%s** loaded successfully." % extension)
+
+        await ctx.send("\n".join(log))
+        print("%s RELOADED THE BOTS MODULES." % ctx.author.name.upper())
+    else:
+        try:
+            bot.reload_extension(cog)
+            await ctx.send("**%s** reloaded successfully." % cog)
+        except:
+            bot.load_extension(cog)
+            await ctx.send("**%s** loaded successfully." % cog)
+        print("%s RELOADED THE %s MODULE." % (ctx.author.name.upper(), cog.upper()))
+
+
+with open("T:/all/2ms2a_b_creds.txt", "r") as token:
+    bot.run(token.read())
